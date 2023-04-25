8367c5c22650e059ace06fabd32127db7e0b596b
sahith2307 <sai.sahith@angularminds.in>
2023-04-25 16:08:59 +0530

    none

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SahpesComponent } from './sahpes/sahpes.component';
+import { RefractionComponent } from './refraction/refraction.component';
 
 @NgModule({
-  declarations: [AppComponent, SahpesComponent],
+  declarations: [AppComponent, SahpesComponent, RefractionComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/refraction/refraction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-refraction',
+  templateUrl: './refraction.component.html',
+  styleUrls: ['./refraction.component.scss']
+})
+export class RefractionComponent {
+
+}
